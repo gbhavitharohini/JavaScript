10b6982f60278ee2d8e322d0bcb8bedbfabcf863
gbhavitharohini <bhavitha-rohini.gouda@capgemini.com>
2024-09-10 12:04:01 +0530

    Add files via upload

---
+++ Conditions/if else prize.js
@@ -0,0 +1,10 @@
+var place = 'Second';
+if (place == 'first'){
+    console.log('Gold')
+} else if(place == 'Second'){
+    console.log('Silver')
+} else if (place == 'third'){
+    console.log('bronze')
+} else {
+    console.log('no medal')
+}
