eaad2119e4edfd31f8a72e55055813f64bce2f29
gbhavitharohini <bhavitha-rohini.gouda@capgemini.com>
2024-09-10 12:19:51 +0530

    Add files via upload

---
+++ Conditions/if else ex.js
@@ -0,0 +1,12 @@
+var light = 'xyz';
+
+if(light == "green") {
+    console.log("Drive")
+} else if (light == "orange") {
+    console.log("Get ready")
+} else if (light == "red") {
+    console.log("Don't drive")
+} else {
+    
+    console.log("The light is not green, orange, or red");
+}
